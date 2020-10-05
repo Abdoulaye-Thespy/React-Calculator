ded9c60bbe23424c988684f22f2aeaa80db581bf
SafaErden <safa@DESKTOP-F1CKM19.localdomain>
2020-10-05 19:16:24 +0300

    fix linter errors

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,21 +1,22 @@
 const Big = require('big.js');
 
 const operate = (numberOne, numberTwo, operation) => {
-	let one = new Big(numberOne);
-	let two = new Big(numberTwo);
-	switch (operation) {
-		case '+':
-			return one.plus(two).toString();
-		case '-':
-			return one.minus(two).toString();
-		case 'x':
-			return one.times(two).toString();
-		case '÷':
-			if (two != 0) {
-				return one.div(two).toString();
-			} else {
-				return undefined;
-			}
-	}
+  const one = new Big(numberOne);
+  const two = new Big(numberTwo);
+  switch (operation) {
+    case '+':
+      return one.plus(two).toString();
+    case '-':
+      return one.minus(two).toString();
+    case 'x':
+      return one.times(two).toString();
+    case '÷':
+      if (two !== 0) {
+        return one.div(two).toString();
+      }
+      return undefined;
+    default:
+      return 0;
+  }
 };
 export default operate;
