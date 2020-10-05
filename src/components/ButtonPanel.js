87b4b1733030ba841d1fdd353be73e64b4bf6a99
SafaErden <safa@DESKTOP-F1CKM19.localdomain>
2020-10-05 16:58:42 +0300

    pass classes as props

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -4,34 +4,34 @@ import Button from './Button';
 function ButtonPanel() {
 	return (
 		<div>
-			<div>
+			<div className="groupButtons">
 				<Button buttonName="AC" className="gray" />
 				<Button buttonName="+/-" className="gray" />
 				<Button buttonName="%" className="gray" />
-				<Button buttonName="÷" className="orange" />
+				<Button buttonName="÷" />
 			</div>
-			<div>
+			<div className="groupButtons">
 				<Button buttonName="7" className="gray" />
 				<Button buttonName="8" className="gray" />
 				<Button buttonName="9" className="gray" />
-				<Button buttonName="x" className="orange" />
+				<Button buttonName="x" />
 			</div>
-			<div>
+			<div className="groupButtons">
 				<Button buttonName="4" className="gray" />
 				<Button buttonName="5" className="gray" />
 				<Button buttonName="6" className="gray" />
-				<Button buttonName="-" className="orange" />
+				<Button buttonName="-"  />
 			</div>
-			<div>
+			<div className="groupButtons">
 				<Button buttonName="1" className="gray" />
 				<Button buttonName="2" className="gray" />
 				<Button buttonName="3" className="gray" />
-				<Button buttonName="+" className="orange" />
+				<Button buttonName="+" />
 			</div>
-			<div>
+			<div className="groupButtons">
 				<Button buttonName="0" className="fifty gray" />
 				<Button buttonName="." className="gray" />
-				<Button buttonName="=" className="orange" />
+				<Button buttonName="="  />
 			</div>
 		</div>
 	);
